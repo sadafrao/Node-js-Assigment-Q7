7cedfe595bb216b200de93b64c7f811323c43cc3
sadaf sohail <159582784+sadafrao@users.noreply.github.com>
2024-02-18 06:06:27 -0800

    Add files via upload

---
+++ Q7.js
@@ -0,0 +1,12 @@
+//Q 7. Number Eight : write addition ,subtraction, multiplication,,and division
+//operration that each result in the number 8.Be sure to enclose your option in print statment to see the results.
+//1.Addition
+// let a: number = 10
+// let b: number = 5
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b );
+// console.log(a / b );
+var a = 10;
+var b = 2;
+console.log(a * b);
---
+++ Q7.ts
@@ -0,0 +1,15 @@
+//Q 7. Number Eight : write addition ,subtraction, multiplication,,and division
+//operration that each result in the number 8.Be sure to enclose your option in print statment to see the results.
+
+
+//1.Addition
+
+// let a: number = 10
+// let b: number = 5
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b );
+// console.log(a / b );
+
+
